e14fd00cd98d87df886c0a175eebd17eb03a947c
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-09-26 13:37:01 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import TaskPage from "./pages/TaskPage.jsx";
 
 const router = createBrowserRouter([
